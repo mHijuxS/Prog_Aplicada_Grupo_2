713b37141c14cd4cb72c0ae70f4b0ef1c60349da
mHiluxS <108304662+mHijuxS@users.noreply.github.com>
2023-04-03 21:18:58 -0300

    Removendo comentarios inuteis

--- algorithms/Projeto1/solucao_complementar.py
+++ algorithms/Projeto1/solucao_complementar.py
@@ -233,41 +233,19 @@ class Projeto1SolucaoComplementar(QgsProcessingAlgorithm):
         return {}
 
     def name(self):
-        """
-        Returns the algorithm name, used for identifying the algorithm. This
-        string should be fixed for the algorithm, and must not be localised.
-        The name should be unique within each provider. Names should contain
-        lowercase alphanumeric characters only and no spaces or other
-        formatting characters.
-        """
         return 'Solução Complementar do Projeto 1'
 
     def displayName(self):
-        """
-        Returns the translated algorithm name, which should be used for any
-        user-visible display of the algorithm name.
-        """
         return self.tr(self.name())
 
     def group(self):
-        """
-        Returns the name of the group this algorithm belongs to. This string
-        should be localised.
-        """
         return self.tr(self.groupId())
 
     def groupId(self):
-        """
-        Returns the unique ID of the group this algorithm belongs to. This
-        string should be fixed for the algorithm, and must not be localised.
-        The group id should be unique within each provider. Group id should
-        contain lowercase alphanumeric characters only and no spaces or other
-        formatting characters.
-        """
         return 'Projeto 1'
 
     def tr(self, string):
-        return QCoreApplication.translate('Processing', string)
+        return QCoreApplication.translate('Processando', string)
 
     def createInstance(self):
         return Projeto1SolucaoComplementar()
